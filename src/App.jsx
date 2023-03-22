48286e5a12fec5ca7208fb459af9ec07f3b543b0
Orel-Viktor <OrelViktor93@gmail.com>
2023-03-22 11:55:13 +0200

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import './styles/App.css'
-
+import React from 'react';
+import './styles/App.css';
+const testPrettier = { mensagem: 'isso é um teste do prettier' };
 function App() {
-  return(
-    <div></div>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/main.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/main.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
