caa3e79ced488bc39bf520b8cf8a55db99e75a5a
Orel-Viktor <OrelViktor93@gmail.com>
2023-03-22 12:08:21 +0200

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles/App.css';
-const testPrettier = { mensagem: 'isso é um teste do prettier' };
+const testPrettier = { mensagem: 'Sisso é um teste do prettier' };
 function App() {
-  return <div></div>;
+   return <div></div>;
 }
 
 export default App;
