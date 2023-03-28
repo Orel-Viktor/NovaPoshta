0c8645e7366d093f0b3725bb7440e9b0bee63d86
Orel-Viktor <OrelViktor93@gmail.com>
2023-03-28 15:50:26 +0300

    test

--- src/ui/components/TrackingHistory.jsx
+++ src/ui/components/TrackingHistory.jsx
@@ -1,35 +1,45 @@
 // Core
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectorsTrackingPackage } from '../../engine/core/tracking-package/selectors';
 // Parts
 import { Box } from '@mui/material';
 
-function TrackingInfoInner(props) {
+function TrackingHistoryInner(props) {
    const { data } = props;
+   const trackingNumbers = [];
+
+   useEffect(() => {
+      trackingNumbers.push(data.Number);
+   }, []);
+   console.log(trackingNumbers);
+   //    localStorage.setItem('trackingNumber', [JSON.stringify(data.Number)]);
    return (
       <>
          <Box sx={{ fontSize: '30px', color: 'primary.main' }}>
-            {data.Status}
+            {data.Number}
          </Box>
       </>
    );
 }
 
-export function TrackingInfo() {
+export function TrackingHistory() {
    const trackingItems = useSelector(selectorsTrackingPackage.items);
    const trackingItemsData = trackingItems.data;
    return (
       <>
-         <Box component="h2">INFO</Box>
+         <Box component="h2">Текущие накладные</Box>
          {typeof trackingItemsData !== 'undefined' ? (
             trackingItemsData.map((data, id) => {
                return (
-                  <TrackingInfoInner key={id} data={data}></TrackingInfoInner>
+                  <TrackingHistoryInner
+                     key={id}
+                     data={data}
+                  ></TrackingHistoryInner>
                );
             })
          ) : (
-            <div>Статус Посылки</div>
+            <div>History</div>
          )}
       </>
    );
--- src/ui/pages/Main.jsx
+++ src/ui/pages/Main.jsx
@@ -4,19 +4,14 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { TrakingForm } from '../components/_Forms/TrakingForm';
 import { TrackingInfo } from '../components/TrackingInfo';
+import { TrackingHistory } from '../components/TrackingHistory';
 
 export function Main() {
    return (
       <Box component="div">
          <TrakingForm />
          <TrackingInfo />
-
-         <Box
-            component="div"
-            sx={{ fontSize: '40px', lineHeight: 1, margin: 8 }}
-         >
-            история ТТН
-         </Box>
+         <TrackingHistory />
       </Box>
    );
 }
