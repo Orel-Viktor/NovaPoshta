7c118655e9c7a80f8d6db8acb7ed3ab96f4b0ecd
Orel-Viktor <orelviktor93@gmail.com>
2024-01-26 11:52:51 +0200

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,30 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
    plugins: [react(), eslint()],
    base: '/NovaPoshta/',
+   build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+   },
+   pages: [
+      {
+         // Имя страницы
+         name: 'home',
+         // Входной файл для этой страницы
+         entry: 'src/ui/pages/Main/index.js',
+         // Шаблон, который будет использоваться для создания HTML-файла
+         template: 'public/index.html',
+         // Путь для сохранения HTML-файла
+         filename: 'NovaPoshta/index.html',
+         // Заголовок страницы
+         title: 'Home Page',
+      },
+      {
+         name: 'admissionsList',
+         entry: 'src/ui/pages/AdmissionsList/index.js',
+         template: 'public/index.html',
+         filename: 'NovaPoshta/admissionsList/index.html',
+         title: 'Admissions List Page',
+      },
+      // Добавьте дополнительные объекты для других страниц
+   ],
 });
