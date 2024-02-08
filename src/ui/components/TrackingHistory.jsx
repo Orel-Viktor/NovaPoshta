57a46826693983936d84c93f10d00d3351ad262e
Orel-Viktor <OrelViktor93@gmail.com>
2024-02-08 20:55:15 +0300

    fix src in icons on button

--- src/ui/components/TrackingHistory.jsx
+++ src/ui/components/TrackingHistory.jsx
@@ -23,7 +23,7 @@ function DelButton(props) {
          <IconDelete />
          <img
             className="tracking-history__mobile-delete-icon root"
-            src="/src/ui/components/_Icons/delete-mobile-icon.png"
+            src="./src/ui/components/_Icons/delete-mobile-icon.png"
             alt="видалити  ТТН"
          />
       </Button>
@@ -108,7 +108,7 @@ function MobileTrackingHistoryInner(props) {
                         role="button"
                         onClick={() => coppyNumber(data.Number)}
                         className="tracking-history__mobile-coppy-image"
-                        src="/src/ui/components/_Icons/coppy-number-image.png"
+                        src="./src/ui/components/_Icons/coppy-number-image.png"
                         alt="копіювати ТТН"
                      />
                   </div>
