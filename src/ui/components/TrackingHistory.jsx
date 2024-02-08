8de6fc6d27e114d66402144e6ee35e087d7b347d
Orel-Viktor <OrelViktor93@gmail.com>
2024-02-08 22:48:08 +0300

    fix

--- src/ui/components/TrackingHistory.jsx
+++ src/ui/components/TrackingHistory.jsx
@@ -23,7 +23,7 @@ function DelButton(props) {
          <IconDelete />
          <img
             className="tracking-history__mobile-delete-icon root"
-            src="src/ui/components/icons/deleteMobileIcon.PNG"
+            src="./src/ui/components/icons/deleteMobileIcon.PNG"
             alt="видалити  ТТН"
          />
       </Button>
@@ -108,7 +108,7 @@ function MobileTrackingHistoryInner(props) {
                         role="button"
                         onClick={() => coppyNumber(data.Number)}
                         className="tracking-history__mobile-coppy-image"
-                        src="src/ui/components/icons/coppyNumberImage.PNG"
+                        src="./src/ui/components/icons/coppyNumberImage.png"
                         alt="копіювати ТТН"
                      />
                   </div>
