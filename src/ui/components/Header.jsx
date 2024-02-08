8be519a655f0d476d134052df279c7dce9922016
Orel-Viktor <OrelViktor93@gmail.com>
2024-02-08 21:14:56 +0300

    fix header link state

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -1,5 +1,5 @@
 // Core
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Parts
 import Stack from '@mui/material/Stack';
 // import { Button } from './Button';
@@ -9,10 +9,18 @@ import { routes } from '../../engine/config/routers';
 
 export function Header() {
    const [activeLink, setActiveLink] = useState('link1');
-
    const handleClick = (link) => {
       setActiveLink(link);
+      localStorage.setItem('activeLink', link);
    };
+
+   useEffect(() => {
+      const storedLink = localStorage.getItem('activeLink');
+      if (storedLink) {
+         setActiveLink(storedLink);
+      }
+   }, []);
+
    return (
       <Stack spacing={2} direction="row">
          <Link
